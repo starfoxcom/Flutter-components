1455b5b16dfc88d215aa573cbbc6edd86eb7ca87
Alex Zamudio <alexzamudio_11@hotmail.com>
2021-02-10 09:51:36 -0600

    Add background image component

---
+++ backgroundImageComponent.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+/// Class component for a background image.
+class BackgroundImageComponent extends StatelessWidget {
+  /// The background image of this component.
+  final String image;
+
+  /// The begin offset alignment of this component, for linear gradient.
+  final AlignmentGeometry beginAlignment;
+
+  /// The end offset alignment of this component, for linear gradient.
+  final AlignmentGeometry endAlignment;
+
+  /// The begin offset color alignment of this component, for linear gradient.
+  final Color beginColor;
+
+  /// The end offset color alignment of this component, for linear gradient.
+  final Color endColor;
+
+  /// The blend mode of this component, for linear gradient.
+  final BlendMode gradientBlendMode;
+
+  /// The box fit for the image of this component.
+  final BoxFit imageFit;
+
+  /// The image color filter of this component.
+  final Color imageColorFilter;
+
+  /// The image blend mode of this component.
+  final BlendMode blendModeImage;
+
+  const BackgroundImageComponent({
+    Key key,
+    @required this.image,
+    this.beginAlignment = Alignment.bottomCenter,
+    this.endAlignment = Alignment.bottomCenter,
+    this.beginColor = Colors.black,
+    this.endColor = Colors.transparent,
+    this.gradientBlendMode = BlendMode.darken,
+    this.imageFit = BoxFit.cover,
+    this.imageColorFilter = Colors.transparent,
+    this.blendModeImage = BlendMode.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) => LinearGradient(
+        begin: beginAlignment,
+        end: endAlignment,
+        colors: [beginColor, endColor],
+      ).createShader(rect),
+      blendMode: gradientBlendMode,
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: imageFit,
+            colorFilter: ColorFilter.mode(
+              imageColorFilter,
+              blendModeImage,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
