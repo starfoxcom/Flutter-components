c819b3010fdfc4fc2c8e5e029145bb84fdc20caa
Jorge Zamudio <alexzamudio_11@hotmail.com>
2021-08-24 02:04:52 -0500

    Fix component for null safety and add on hover functionality properties

--- actionButtonComponent.dart
+++ actionButtonComponent.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-/// Class component for a flat design action button.
+/// Class component for a design action button.
 ///
 /// This component class depends of `font_awesome_flutter.dart`, consider adding
 ///  it to your dependencies on the `pubspec.yaml` file of the project.
-class FlatActionButtonComponent extends StatelessWidget {
+class ActionButtonComponent extends StatelessWidget {
   /// The left padding of this component.
   final double leftPadding;
 
@@ -21,21 +21,24 @@ class FlatActionButtonComponent extends StatelessWidget {
   /// The symmetric vertical padding of this component.
   ///
   /// This has priority over the [topPadding] and [bottomPadding] values.
-  final double verticalPadding;
+  final double? verticalPadding;
 
   /// The symmetric horizontal padding of this component.
   ///
   /// This has proprity over the [leftPadding] and [rightPadding] values.
-  final double horizontalPadding;
+  final double? horizontalPadding;
 
   /// The spacing between the [prefixIcon], [labelText] and [suffixIcon] of this component.
   final double contentSpacing;
 
   /// The width of this component.
-  final double width;
+  final double? width;
 
   /// The height of this component.
-  final double height;
+  final double? height;
+
+  /// The elevation of this component.
+  final double? elevation;
 
   /// The text to display on this component.
   final String labelText;
@@ -43,10 +46,17 @@ class FlatActionButtonComponent extends StatelessWidget {
   /// The text color of this component.
   final Color textColor;
 
+  /// The text [onHover] color of this component.
+  final Color hoverTextColor;
+
   /// The background color of this component.
   final Color backgroundColor;
 
-  /// The highlight (on pressed) color of this component.
+  /// The highlight [onHover] color of this component.
+  final Color hoverColor;
+
+  /// The highlight [onPressed] color of this component. This also establishes
+  /// the [borderColor] for the [onPressed] state.
   final Color highlightColor;
 
   /// The border color of this component.
@@ -55,8 +65,14 @@ class FlatActionButtonComponent extends StatelessWidget {
   /// The border radius (circular) of this component.
   final double borderRadius;
 
+  /// The border radius (circular) [onPressed] of this component.
+  final double? pressedBorderRadius;
+
   /// The border width of this component.
   final double borderWidth;
+
+  /// The border width [onPressed] of this component.
+  final double? pressedBorderWidth;
 
   /// Sets the prefix icon visible of this component.
   final bool isPrefixIconVisible;
@@ -82,8 +98,8 @@ class FlatActionButtonComponent extends StatelessWidget {
   /// The [onLongPressed] callback function of this component.
   final void Function() onLongPressedCallback;
 
-  const FlatActionButtonComponent({
-    Key key,
+  const ActionButtonComponent({
+    Key? key,
     this.leftPadding = 0,
     this.topPadding = 0,
     this.rightPadding = 0,
@@ -93,45 +109,83 @@ class FlatActionButtonComponent extends StatelessWidget {
     this.contentSpacing = 10,
     this.width,
     this.height,
+    this.elevation = 0,
     this.labelText = "Label text",
     this.textColor = Colors.black,
+    this.hoverTextColor = Colors.white,
     this.backgroundColor = Colors.white,
     this.highlightColor = Colors.grey,
-    this.borderColor = Colors.blueAccent,
+    this.hoverColor = Colors.black,
+    this.borderColor = Colors.black,
     this.borderRadius = 0,
     this.borderWidth = 2,
+    this.pressedBorderRadius,
+    this.pressedBorderWidth,
     this.isPrefixIconVisible = false,
     this.isSuffixIconVisible = false,
     this.prefixIcon = FontAwesomeIcons.icons,
     this.suffixIcon = FontAwesomeIcons.icons,
     this.fontSize = 24,
     this.iconSize = 24,
-    @required this.onPressedCallback,
-    @required this.onLongPressedCallback,
+    required this.onPressedCallback,
+    required this.onLongPressedCallback,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(
-        left: (horizontalPadding != null) ? horizontalPadding : leftPadding,
-        top: (verticalPadding != null) ? verticalPadding : topPadding,
-        right: (horizontalPadding != null) ? horizontalPadding : rightPadding,
-        bottom: (verticalPadding != null) ? verticalPadding : bottomPadding,
+        left: horizontalPadding ?? leftPadding,
+        top: verticalPadding ?? topPadding,
+        right: horizontalPadding ?? rightPadding,
+        bottom: verticalPadding ?? bottomPadding,
       ),
       child: SizedBox(
         width: width,
         height: height,
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            primary: highlightColor,
-            backgroundColor: backgroundColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(borderRadius),
+        child: ElevatedButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.resolveWith(
+              (states) {
+                return states.contains(MaterialState.hovered)
+                    ? hoverTextColor
+                    : textColor;
+              },
             ),
-            side: BorderSide(
-              width: borderWidth,
-              color: borderColor,
+            backgroundColor: MaterialStateProperty.resolveWith((states) =>
+                states.contains(MaterialState.pressed)
+                    ? hoverColor
+                    : backgroundColor),
+            overlayColor: MaterialStateProperty.resolveWith(
+              (states) {
+                return states.contains(MaterialState.pressed)
+                    ? highlightColor
+                    : hoverColor;
+              },
+            ),
+            elevation: MaterialStateProperty.all(elevation),
+            shape: MaterialStateProperty.resolveWith(
+              (states) {
+                return states.contains(MaterialState.pressed)
+                    ? RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          pressedBorderRadius ?? borderRadius,
+                        ),
+                        side: BorderSide(
+                          width: pressedBorderWidth ?? borderWidth,
+                          color: highlightColor,
+                        ),
+                      )
+                    : RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          borderRadius,
+                        ),
+                        side: BorderSide(
+                          width: borderWidth,
+                          color: borderColor,
+                        ),
+                      );
+              },
             ),
           ),
           onPressed: () {
@@ -149,17 +203,25 @@ class FlatActionButtonComponent extends StatelessWidget {
                       prefixIcon,
                       size: iconSize,
                     )
-                  : Container(width: 0, height: 0),
+                  : Container(
+                      width: 0,
+                      height: 0,
+                    ),
               Text(
                 labelText,
-                style: TextStyle(fontSize: fontSize, color: textColor),
+                style: TextStyle(
+                  fontSize: fontSize,
+                ),
               ),
               (isSuffixIconVisible)
                   ? Icon(
                       suffixIcon,
                       size: iconSize,
                     )
-                  : Container(width: 0, height: 0),
+                  : Container(
+                      width: 0,
+                      height: 0,
+                    ),
             ],
           ),
         ),
